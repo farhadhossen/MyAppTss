b324153691feaea2ec4dabb5d389fbd5055581fd
farhadhossen <imfarhad01@gmail.com>
2021-06-08 12:41:06 +0600

    Initial commit

---
+++ app/src/main/java/com/rdtl/job/chakri/myapp/MyAdapter.java
@@ -0,0 +1,79 @@
+package com.rdtl.job.chakri.myapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    String data1[], data2[];
+    int images[];
+    Context context;
+
+
+
+    public MyAdapter(Context ct, String s1[], String s2[], int img[]){
+
+        context = ct;
+        data1 = s1;
+        data2 = s2;
+        images = img;
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        holder.myText1.setText(data1[position]);
+        holder.myText2.setText(data2[position]);
+        holder.myImage.setImageResource(images[position]);
+
+        holder.mainlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "hi", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView myText1, myText2;
+        ImageView myImage;
+        ConstraintLayout mainlayout;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            myText1 = itemView.findViewById(R.id.idT1);
+            myText2 = itemView.findViewById(R.id.idT2);
+            myImage = itemView.findViewById(R.id.idMyImg);
+            mainlayout = itemView.findViewById(R.id.idmainlayout);
+
+
+        }
+    }
+}
